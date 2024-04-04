72d2dfd8647f983fcf66c63971c2e4a4ca379c22
venureddygithub <venuetta104254@gmail.com>
2024-04-04 03:21:25 +0000

    solution

---
+++ src/components/textEdit/index.js
@@ -0,0 +1,16 @@
+import {Component} from './react'
+import './index.css'
+
+class textEdit extends Component {
+  render() {
+    return (
+      <div className="bg-container">
+        <div className="container">
+          <h1>Editable Text Input</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default textEdit
